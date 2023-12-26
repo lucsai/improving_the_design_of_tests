b637738790ce01167b9cdcd503fffcdeca3eb684
lucsai <lucsaitan@gmail.com>
2023-12-27 00:21:26 +0700

    Task: content independence, looking for a bug https://stepik.org/lesson/201964/step/4?unit=176022

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,15 @@
 from .pages.product_page import PageObject
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = PageObject(browser, url)
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+    page = PageObject(browser, link)
     page.open()
-    page.should_be_newYear_url()
+    #page.should_be_newYear_url()
     page.button_add_to_basket_is_present()
     page.add_product_to_basket()
     page.should_be_message_product_has_been_added_to_basket()

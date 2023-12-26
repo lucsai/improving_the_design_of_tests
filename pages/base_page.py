d85cc16072c02429225aff7fc33b377ea52cf58d
lucsai <lucsaitan@gmail.com>
2023-12-26 21:24:15 +0700

    modified base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 import math
 

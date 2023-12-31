9092b848c24d5cbda037fe42d0026b860d2a956b
lucsai <lucsaitan@gmail.com>
2024-01-01 01:56:43 +0700

    modified test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,12 @@
+from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 
 link = "https://selenium1py.pythonanywhere.com"
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    page = BasketPage(browser, link)
+    page = MainPage(browser, link)
     page.open()
     page.go_to_basket_page()
-    page.should_be_empty_basket()
-    page.should_be_message_about_empty_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_message_about_empty_basket()

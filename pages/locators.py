d8796facd73488fff2531291a4f8efa018070093
lucsai <lucsaitan@gmail.com>
2024-01-01 18:15:43 +0700

    modified test_main_page.py, test_product_page.py, locators.py added login_page.py, pytest.ini

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,19 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    VIEW_BASKET = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
+    NOT_EMPTY_BASKET = (By.CSS_SELECTOR, "#basket_formset")
+    MESSAGE_ABOUT_EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner")
+
+class LoginPageLocators():
+    LOGIN_USERNAME = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
@@ -7,10 +21,3 @@ class ProductPageLocators():
     SUCCESS_MESSAGE = (By.CSS_SELECTOR,"div.alert:first-child")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
     PRODUCT_PRICE_IN_MESSAGE_2 = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
-
-class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    VIEW_BASKET = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
-    NOT_EMPTY_BASKET = (By.CSS_SELECTOR, "#basket_formset")
-    MESSAGE_ABOUT_EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner")
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,8 @@
 from .pages.product_page import PageObject
 from .pages.basket_page import BasketPage
-
 import pytest
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
@@ -12,7 +11,6 @@ def test_guest_can_add_product_to_basket(browser, link):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = PageObject(browser, link)
     page.open()
-    #page.should_be_newYear_url()
     page.button_add_to_basket_is_present()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
@@ -23,7 +21,7 @@ def test_guest_can_add_product_to_basket(browser, link):
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 link2 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = PageObject(browser, link)
@@ -32,14 +30,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message(browser):
     page = PageObject(browser, link)
     page.open()
     page.should_not_be_success_message()
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = PageObject(browser, link)
@@ -48,13 +46,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.test_message_disappeared_after_adding_product_to_basket()
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     page = PageObject(browser, link2)
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = PageObject(browser, link2)
     page.open()
